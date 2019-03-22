421086e063a84c009fa54679302ea12715af80ee
paupaugoz <37976584+paupaugoz@users.noreply.github.com>
2019-03-22 22:48:01 +0800

    add

--- index.js
+++ index.js
@@ -27,6 +27,27 @@ let employeeData = [{
  "action": "Action"
 }];
 
+let projectsData = [{
+  "id": "1001",
+  "projectName": "Fujjang International Corp.",
+  "dateCreated": "Feb 23, 2019",
+  "dateFinished": "Feb 24, 2021",
+  "noofUnits": "5",
+  "eic": "Roland",
+  "status": "Ongoing",
+  "action": "Action"
+},
+{
+  "id": "1001",
+  "projectName": "Fujjang International Corp.",
+  "dateCreated": "Feb 23, 2019",
+  "dateFinished": "Feb 24, 2021",
+  "noofUnits": "5",
+  "eic": "Roland",
+  "status": "Ongoing",
+  "action": "Action"
+}];
+
 let poData = [{
  "id": "1001",
  "supplier": "Supplier Name",
@@ -125,6 +146,38 @@ let poData = [{
  "action": "Action"
 }];
 
+
+let poviewData = [{
+ "category": "1001",
+ "description": "Placeholder",
+ "quantity": "Aug 11, 2018",
+ "unit": "Sam Malone",
+ "unitCost": "Aug 11, 2018",
+  "amount": "Fullfilled"
+
+},{
+ "category": "1002",
+ "description": "Supplier Name",
+ "quantity": "Aug 11, 2018",
+ "unit": "Sam Malone",
+ "unitCost": "Aug 11, 2018",
+  "amount": "Pending"
+},{
+ "category": "1003",
+ "description": "Supplier Name",
+ "quantity": "Feb 11, 2019",
+ "unit": "Sam Malone",
+ "unitCost": "Fev 11, 2019",
+  "amount": "Cancellend"
+},{
+ "category": "1001",
+ "description": "Supplier Name",
+ "quantity": "Aug 11, 2018",
+ "unit": "Sam Malone",
+ "unitCost": "Aug 11, 2018",
+  "amount": "Fullfilled"
+}];
+
 let materialData = [{
  "id": "1001",
  "material": "Paper",
@@ -142,6 +195,40 @@ let materialData = [{
  "action": "Action"
 }];
 
+let projectsColumns = [{
+  "name": "id",
+  "value": "ID",
+  "editable": false
+},{
+  "name": "projectName",
+  "value": "Project Name",
+  "editable": false
+},{
+  "name": "dateCreated",
+  "value": "Date Created",
+  "editable": false
+},{
+  "name": "dateFinished",
+  "value": "Date Finished",
+  "editable": false
+},{
+  "name": "noofUnits",
+  "value": "No. of Units",
+  "editable": false
+},{
+  "name": "eic",
+  "value": "EIC",
+  "editable": false
+},{
+  "name": "status",
+  "value": "Status",
+  "editable": false
+},{
+  "name": "action",
+  "value": "Action",
+  "editable": true
+}
+]
 let employeeColumns = [{
  "name": "id",
  "value": "ID",
@@ -212,6 +299,32 @@ let poColumns = [{
  "editable": true
 }];
 
+let poviewColumns = [{
+ "name": "category",
+ "value": "Category",
+ "editable": false
+},{
+ "name": "description",
+ "value": "Description",
+ "editable": false
+},{
+ "name": "quantity",
+ "value": "Quantity",
+ "editable": false
+},{
+ "name": "unit",
+ "value": "Unit",
+ "editable": false
+},{
+ "name": "unitCost",
+ "value": "Unit Cost",
+ "editable": false
+},{
+ "name": "amount",
+ "value": "Amount",
+ "editable": false
+}];
+
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 
 app.set('view engine', 'handlebars');
@@ -219,19 +332,31 @@ app.set('view engine', 'handlebars');
 app.use('/static', express.static('public'));
 
 app.get('/', (req, res) => {
-  res.render('employees', { active: {employees: true}, data: employeeData, columns: employeeColumns, pageHeader: "Employees"});
+  res.render('management/employees', { active: {employees: true}, data: employeeData, columns: employeeColumns, pageHeader: "Employees"});
+});
+
+app.get('/projects', (req, res) => {
+  res.render('management/projects', { active: {projects: true}, data: projectsData, columns: projectsColumns, pageHeader: "Projects"});
+});
+
+app.get('/createprojects', (req, res) => {
+  res.render('management/projects', { active: {projects: true}, pageHeader: "Create a New Project"});
 });
 
 app.get('/po', (req, res) => {
-  res.render('po', { active: {PO: true}, data: poData, columns: poColumns, pageHeader: "Purchase Orders"});
+  res.render('management/po', { active: {PO: true}, data: poData, columns: poColumns, pageHeader: "Purchase Orders"});
+});
+
+app.get('/viewapo', (req, res) => {
+  res.render('management/viewapo', { active: {PO: true}, data: poviewData, columns: poviewColumns, pageHeader: "Purchase Orders"});
 });
 
 app.get('/materials', (req, res) => {
-  res.render('materials', { active: {materials: true}, data: materialData, columns: materialColumns, pageHeader: "Materials"});
+  res.render('management/materials', { active: {materials: true}, data: materialData, columns: materialColumns, pageHeader: "Materials"});
 });
 
 app.get('/creatematerials', (req, res) => {
-  res.render('creatematerials', { active: {creatematerials: true}});
+  res.render('management/creatematerials', { active: {creatematerials: true}});
 });
 
 
